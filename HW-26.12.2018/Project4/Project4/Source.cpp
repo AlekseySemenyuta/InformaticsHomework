501014eea3d1fdb8b0d98a9e065c4c92abeb7455
AlekseySemenyuta <alekseysemenyuta@mail.ru>
2018-12-27 00:37:37 +0300

    .

--- HW-26.12.2018/Project4/Project4/Source.cpp
+++ HW-26.12.2018/Project4/Project4/Source.cpp
@@ -1,1 +1,18 @@
 //Реализуйте вычисление значения полинома в точке посредством схемы Горнера.
+#include <iostream> 
+using namespace std;
+int main() {
+	unsigned int x, y;
+	cin >> x >> y;
+	int* a = new int[x + 1];
+	for (unsigned int i = 0; i < x + 1; i++) {
+		cin >> a[i];
+	}
+	int b = a[x];
+	for (int i = x - 1; i >= 1; i--) {
+		b = a[i] + b * y;
+	}
+	cout << a[0] + b * y;
+	system("pause");
+	return 0;
+}
